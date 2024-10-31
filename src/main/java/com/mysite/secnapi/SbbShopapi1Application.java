c39ddaba4cfc254d824516ec49893f8f330ea499
mila811230 <bluegalaxy1230@gmail.com>
2024-10-31 10:09:29 +0900

    프로젝트 시작

---
+++ src/main/java/com/mysite/secnapi/SbbShopapi1Application.java
@@ -0,0 +1,13 @@
+package com.mysite.secnapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbShopapi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbbShopapi1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysite/secnapi/SbbShopapi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.secnapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbShopapi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
